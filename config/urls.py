2eebc8464ea72901b525604b99537a46cb3bdc7d
mhmd <sherkatfiles0214@yahoo.com>
2023-05-07 11:13:43 +0330

    the problem in login solved (it was an spelling mistake :( )

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/' ,TemplateView.as_view(template_name= 'home.html'), name='home' ),
+    path('home/' , TemplateView.as_view(template_name= 'home.html'), name='home' ),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
 
