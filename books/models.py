a340ccbf6b215b73f50193fbf6a493d7a02da403
mhmd <sherkatfiles0214@yahoo.com>
2023-05-10 17:27:46 +0330

    have some change in model and admin

--- books/models.py
+++ books/models.py
@@ -8,4 +8,4 @@ class Books(models.Model):
     price = models.DecimalField(max_digits=5 , decimal_places=2)
 
     def __str__(self):
-        return self.title
+        return f'{self.author} : {self.title}'
