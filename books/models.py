83c73168385f45b3f49f9531a4f34e0605c2a53e
mhmd <sherkatfiles0214@yahoo.com>
2023-05-10 17:13:46 +0330

    the db for books created and configured for admin

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    author = models.CharField(max_length = 200)
+    price = models.DecimalField(max_digits=5 , decimal_places=2)
+
+    def __str__(self):
+        return self.title
