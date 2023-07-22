d898b2194275d2b90789650fb232e5fcc828b60c
mhmd.s <132132219+just-a-normal-developer@users.noreply.github.com>
2023-07-22 16:43:12 +0330

    Update settings.py
    settings updated due to security risk

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sn&64dv1w)=!h1)14%zz7e^tpot4(hksnp=ll()w4ly=_g49@g'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
