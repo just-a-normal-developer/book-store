434cb780cd1fc8a54e22155ad98fc290509e5f18
mhmd <sherkatfiles0214@yahoo.com>
2023-11-08 16:57:21 +0330

    home view template changed for better design

--- books/models.py
+++ books/models.py
@@ -13,6 +13,7 @@ class Books(models.Model):
     author = models.CharField(max_length = 200)
     price = models.DecimalField(max_digits=5 , decimal_places=2)
     cover = models.ImageField(upload_to = 'BookCover', blank = True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.author} : {self.title}'
